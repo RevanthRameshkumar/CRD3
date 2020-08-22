ef44b574c6e8baf637a4ba74e3c52c0ebaeb70e7
RevGit <rev2021@gmail.com>
2020-08-22 14:22:11 -0700

    mroe code cleanup and changed the the max line to 120 to meet w/ repo's PEP standard

--- tests/summarization/test_needleman_wunsch.py
+++ tests/summarization/test_needleman_wunsch.py
@@ -1,16 +1,21 @@
+import json
+
 import numpy as np
 from numpy.testing import assert_array_equal
-import json
+
 from summarization.needleman_wunsch import NeedlemanWunschAligner, TextChunk
 from utils.temp_utils import calculate_scaled_f1_turn_distribution, nltk_stopword_tokenize
 
+
 def test_build_scoring_matrx():
-    chunks = [TextChunk(id=elem['ID'], text=elem['CHUNK']) for elem in json.loads(open('sample_summary.json', encoding='utf-8').read())]
-    turns = [TextChunk(id=elem['ID'], text=elem['TURN']) for elem in json.loads(open('sample_turns.json', encoding='utf-8').read())]
-    tokenized_chunks= [set(nltk_stopword_tokenize(chunk.text, 2, skip_unigrams=False)) for chunk in chunks]
+    chunks = [TextChunk(id=elem['ID'], text=elem['CHUNK']) for elem in
+              json.loads(open('sample_summary.json', encoding='utf-8').read())]
+    turns = [TextChunk(id=elem['ID'], text=elem['TURN']) for elem in
+             json.loads(open('sample_turns.json', encoding='utf-8').read())]
+    tokenized_chunks = [set(nltk_stopword_tokenize(chunk.text, 2, skip_unigrams=False)) for chunk in chunks]
     tokenized_turns = [set(nltk_stopword_tokenize(turn.text, 2, skip_unigrams=False)) for turn in turns]
 
-    s = NeedlemanWunschAligner.build_scoring_matrx(tokenized_turns, tokenized_chunks, calculate_scaled_f1_turn_distribution)
+    s = NeedlemanWunschAligner.build_scoring_matrx(tokenized_turns, tokenized_chunks,
+                                                   calculate_scaled_f1_turn_distribution)
     sample = np.genfromtxt('sample_score_matrix.csv', delimiter=",")
     assert_array_equal(s, sample)
-
