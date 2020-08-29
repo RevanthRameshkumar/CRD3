7000eecf8aac482e86305373ad46fe84875c9c44
RevanthRameshkumar <revanth.rameshkumar@gmail.com>
2020-08-28 21:08:08 -0700

    nw algorithm function implemented (#9)
    * nw algorithm function implemented
    
    implemented nw algorithm function and added test case
    
    * added extra blank line
    
    Co-authored-by: RevGit <rev2021@gmail.com>

--- tests/summarization/test_needleman_wunsch.py
+++ tests/summarization/test_needleman_wunsch.py
@@ -11,7 +11,7 @@ from crd3.utils.temp_utils import calculate_scaled_f1_turn_distribution, nltk_st
 FILE_PATH = Path(__file__).absolute()
 
 
-def test_build_scoring_matrx():
+def test_build_scoring_matrix():
     sample_summary_path = os.path.join(os.path.dirname(FILE_PATH), 'sample_summary.json')
     sample_turns_path = os.path.join(os.path.dirname(FILE_PATH), 'sample_turns.json')
     sample_score_matrix_path = os.path.join(os.path.dirname(FILE_PATH), 'sample_score_matrix.csv')
@@ -23,7 +23,31 @@ def test_build_scoring_matrx():
     tokenized_chunks = [set(nltk_stopword_tokenize(chunk.text, 2, skip_unigrams=False)) for chunk in chunks]
     tokenized_turns = [set(nltk_stopword_tokenize(turn.text, 2, skip_unigrams=False)) for turn in turns]
 
-    s = NeedlemanWunschAligner.build_scoring_matrx(tokenized_turns, tokenized_chunks,
-                                                   calculate_scaled_f1_turn_distribution)
+    s = NeedlemanWunschAligner.build_scoring_matrix(tokenized_turns, tokenized_chunks,
+                                                    calculate_scaled_f1_turn_distribution)
     sample = np.genfromtxt(sample_score_matrix_path, delimiter=",")
     assert_array_equal(s, sample)
+
+
+def test_needleman_wunsch_algorithm():
+    sample_score_matrix_path = os.path.join(os.path.dirname(FILE_PATH), 'sample_score_matrix.csv')
+    sample_nw_output_path = os.path.join(os.path.dirname(FILE_PATH), 'sample_nw_algorithm_output.json')
+
+    matrix = np.genfromtxt(sample_score_matrix_path, delimiter=",")
+    summary_sequence = list(range(matrix.shape[1]))
+    turn_sequence = list(range(matrix.shape[0]))
+
+    nw_result = NeedlemanWunschAligner.needleman_wunsch_algorithm(turn_sequence, summary_sequence, matrix)
+
+    expected_sequence_1 = []
+    expected_sequence_2 = []
+    expected_scores = []
+
+    for expected_alignment in json.load(open(sample_nw_output_path)):
+        expected_sequence_1.append(expected_alignment['sequence_1'])
+        expected_sequence_2.append(expected_alignment['sequence_2'])
+        expected_scores.append(expected_alignment['score'])
+
+    assert nw_result.sequence_1 == expected_sequence_1
+    assert nw_result.sequence_2 == expected_sequence_2
+    assert nw_result.scores == expected_scores
